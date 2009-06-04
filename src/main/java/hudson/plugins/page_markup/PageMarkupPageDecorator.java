dc3f532ce91187e906a1b3918c09981d79b5a272
jdsiiro <jdsiiro@c9093e8b-e624-0410-814a-e3c5b6d4c2ac>
2009-06-04 16:35:53 +0000

    PageMarkup: Adding licensing statements
    git-svn-id: https://software.sandia.gov/svn/public/fast/hudson/plugins/page-markup/trunk@2010 c9093e8b-e624-0410-814a-e3c5b6d4c2ac

--- src/main/java/hudson/plugins/page_markup/PageMarkupPageDecorator.java
+++ src/main/java/hudson/plugins/page_markup/PageMarkupPageDecorator.java
@@ -1,3 +1,20 @@
+/** PageMarkup: An HTML page markup plugin for Hudson
+ *  Copyright (c) 2008 Sandia Corporation.
+ *  All rights resserved.
+ *
+ * PageMarkup is part of the FAST software project, see
+ *   https://software.sandia.gov/trac/fast
+ *
+ * This software is distributed under the BSD License; for the full text
+ * of the license, see the LICENSE.txt file included with this
+ * distribution.  Under the terms of Contract DE-AC04-94AL85000, there is
+ * a non-exclusive license for use of this work by or on behalf of the
+ * U.S. Government. 
+ *
+ * Created by John D. Siirola <jdsiiro@sandia.gov>
+ * Sandia National Laboratories
+ * 4 June 2009
+ */
 package hudson.plugins.page_markup;
 
 import net.sf.json.JSONObject;
